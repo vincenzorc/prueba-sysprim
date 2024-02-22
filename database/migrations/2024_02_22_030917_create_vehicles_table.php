e0ce4e6a866f01304ef26ac96a8743da28efd38b
Vincenzo <vincenzor.celu@gmail.com>
2024-02-21 23:26:24 -0400

    Model Vehicle

---
+++ database/migrations/2024_02_22_030917_create_vehicles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa',8)->unique();
+            $table->year('year');
+            $table->string('color');
+            $table->date('date_entry');
+
+            $table->unsignedBigInteger('mark_id');
+            $table->foreign('mark_id')->references('id')->on('marks');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
